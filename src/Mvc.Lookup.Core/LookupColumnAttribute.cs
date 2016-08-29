86f00629dd546eae9c9b5fb38371482fd3f7e783
Muchiachio <aldis.rameika@gmail.com>
2016-08-29 14:40:02 +0300

    Not nullable lookup column position
    - Made lookup column position not nullable to allow using negatives
    values for indicating a first column.

--- src/Mvc.Lookup.Core/LookupColumnAttribute.cs
+++ src/Mvc.Lookup.Core/LookupColumnAttribute.cs
@@ -4,7 +4,7 @@ namespace NonFactors.Mvc.Lookup
 {
     public class LookupColumnAttribute : Attribute
     {
-        public Int32? Position { get; set; }
+        public Int32 Position { get; set; }
         public Boolean Hidden { get; set; }
         public String Format { get; set; }
 
--- test/Mvc.Lookup.Tests/Objects/Models/TestModel.cs
+++ test/Mvc.Lookup.Tests/Objects/Models/TestModel.cs
@@ -5,7 +5,7 @@ namespace NonFactors.Mvc.Lookup.Tests.Objects
 {
     public class TestModel
     {
-        [LookupColumn(Hidden = true)]
+        [LookupColumn(-3, Hidden = true)]
         public String Id { get; set; }
 
         [LookupColumn(8)]
@@ -19,7 +19,7 @@ namespace NonFactors.Mvc.Lookup.Tests.Objects
         public String ParentId { get; set; }
 
         [Display(Name = "Date")]
-        [LookupColumn(-3, Format = "{0:d}")]
+        [LookupColumn(3, Format = "{0:d}")]
         public DateTime Date { get; set; }
 
         public String NullableString { get; set; }
