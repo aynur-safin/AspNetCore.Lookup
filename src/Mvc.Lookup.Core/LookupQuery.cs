716eb8538ff328fc2c620ff4754d6c3f5cc7bb79
Muchiachio <aldis.rameika@gmail.com>
2018-09-04 13:00:02 +0300

    Public lookup query API
    - Exposed lookup query api, to reduce code duplication in custom lookups.

--- src/Mvc.Lookup.Core/LookupQuery.cs
+++ src/Mvc.Lookup.Core/LookupQuery.cs
@@ -4,28 +4,32 @@ using System.Linq.Expressions;
 
 namespace NonFactors.Mvc.Lookup
 {
-    internal class LookupQuery : ExpressionVisitor
+    public static class LookupQuery
     {
-        public Boolean Ordered { get; set; }
-
         public static Boolean IsOrdered(IQueryable models)
         {
-            LookupQuery query = new LookupQuery();
-            query.Visit(models.Expression);
+            LookupExpressionVisitor expression = new LookupExpressionVisitor();
+            expression.Visit(models.Expression);
 
-            return query.Ordered;
+            return expression.Ordered;
         }
 
-        protected override Expression VisitMethodCall(MethodCallExpression node)
+        private class LookupExpressionVisitor : ExpressionVisitor
         {
-            if (node.Method.DeclaringType == typeof(Queryable) && (node.Method.Name.StartsWith("OrderBy") || node.Method.Name.StartsWith("ThenBy")))
+            public Boolean Ordered { get; set; }
+
+            protected override Expression VisitMethodCall(MethodCallExpression node)
             {
+                if (node.Method.DeclaringType != typeof(Queryable))
+                    return base.VisitMethodCall(node);
+
+                if (!node.Method.Name.StartsWith("OrderBy"))
+                    return base.VisitMethodCall(node);
+
                 Ordered = true;
 
                 return node;
             }
-
-            return base.VisitMethodCall(node);
         }
     }
 }
---
+++ test/Mvc.Lookup.Tests/Unit/LookupQueryTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace NonFactors.Mvc.Lookup.Tests.Unit
+{
+    public class LookupQueryTests
+    {
+        #region IsOrdered(IQueryable models)
+
+        [Fact]
+        public void IsOrdered_False()
+        {
+            Assert.False(LookupQuery.IsOrdered(new Object[0].OrderBy(model => 0).AsQueryable()));
+        }
+
+        [Fact]
+        public void IsOrdered_True()
+        {
+            Assert.True(LookupQuery.IsOrdered(new Object[0].AsQueryable().OrderBy(model => 0)));
+        }
+
+        #endregion
+    }
+}
